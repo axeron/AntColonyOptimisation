c84a6fec02e451f074e6869187547ec22a59677f
Andreas Svingeseth <axeron91@gmail.com>
2013-04-26 14:57:41 +0100

    starting

---
+++ src/AntColonyDriver.java
@@ -0,0 +1,17 @@
+/**
+ * Class to drive the ant colony optimization program for a minimimum spanning tree
+ * @author Andreas O'Brien Svingeseth (ans23@aber.ac.uk)
+ * @version 0.1
+ * Developing phase 
+ */
+public class AntColonyDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
